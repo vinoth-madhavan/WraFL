ebe087f9ac19cd908b75c9871be61460e68eaa8d
Venkat.R <ramsunvtech@gmail.com>
2024-09-07 19:24:56 +0800

    feat(buttons): add support for background, text color, left and right icon
    1. Add Code Snippet Widget for copy code feature in playground app
    2. Add New Icon Widget backed by FontAwesome

--- lib/src/calls_to_action/button.dart
+++ lib/src/calls_to_action/button.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:wrafl/src/communications/icon.dart';
 
 class WraflButton extends StatelessWidget {
   final String label;
+  final Color? backgroundColor;
+  final Color? textColor;
+  final Widget? leftIcon;
+  final Widget? rightIcon;
+  final bool? hideLabel;
+  final bool? disabled;
+  final bool? showSpinner;
   final VoidCallback onPressed;
 
   const WraflButton({
     Key? key,
+    this.backgroundColor,
+    this.textColor,
+    this.leftIcon,
+    this.rightIcon,
+    this.hideLabel = false,
+    this.disabled = false,
+    this.showSpinner = false,
     required this.label,
     required this.onPressed,
   }) : super(key: key);
@@ -13,8 +29,32 @@ class WraflButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor:
+            backgroundColor ?? Colors.blue, // Default background color if null
+        foregroundColor:
+            textColor ?? Colors.white, // Default text color if null
+      ),
       onPressed: onPressed,
-      child: Text(label),
+      child: Row(
+        mainAxisSize: MainAxisSize.min, // Make the button shrink to fit content
+        children: [
+          if (leftIcon != null) ...[
+            leftIcon!,
+            if (hideLabel == false) const SizedBox(width: 8.0),
+          ],
+          if (hideLabel == false) Text(label),
+          if (showSpinner == true) ...[
+            if (hideLabel == false || leftIcon != null) const SizedBox(width: 8.0),
+            const WraflIcon(
+                icon: FontAwesomeIcons.spinner, color: Colors.blue),
+          ],
+          if (rightIcon != null) ...[
+            if (hideLabel == false) const SizedBox(width: 8.0),
+            rightIcon!,
+          ],
+        ],
+      ),
     );
   }
 }
--- playground/lib/stories/call_to_action/button_stories.dart
+++ playground/lib/stories/call_to_action/button_stories.dart
@@ -1,16 +1,29 @@
-// ignore: implementation_imports
+// ignore_for_file: implementation_imports
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:storybook_flutter/storybook_flutter.dart';
+
+// WraFL Buttons.
 import 'package:wrafl/src/calls_to_action/button.dart';
+import 'package:wrafl/src/communications/icon.dart';
+import 'package:playground/code_snippet.dart';
 
 List<Story> wraflButtonStories = [
   Story(
     name: 'Call To Action/WraflButton/Default',
     builder: (context) {
-      return Center(
+      return CodeSnippet(
+        code: '''WraflButton(
+          label: 'Default Button',
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Button Pressed!')),
+            );
+          },
+        )''',
         child: WraflButton(
-          label: 'Press Me!',
+          label: 'Default Button',
           onPressed: () {
             ScaffoldMessenger.of(context).showSnackBar(
               const SnackBar(content: Text('Button Pressed!')),
@@ -23,17 +36,26 @@ List<Story> wraflButtonStories = [
   Story(
     name: 'Call To Action/WraflButton/Background Color',
     builder: (context) {
-      const color = Colors.blue;
+      const color = Colors.green;
 
-      return Center(
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(backgroundColor: color),
+      return CodeSnippet(
+        code: '''WraflButton(
+          label: 'Green Background Color',
+          backgroundColor: color,
           onPressed: () {
             ScaffoldMessenger.of(context).showSnackBar(
               const SnackBar(content: Text('Button Pressed!')),
             );
           },
-          child: const Text('Press Me!'),
+        )''',
+        child: WraflButton(
+          label: 'Green Background Color',
+          backgroundColor: color,
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Button Pressed!')),
+            );
+          },
         ),
       );
     },
@@ -41,17 +63,165 @@ List<Story> wraflButtonStories = [
   Story(
     name: 'Call To Action/WraflButton/Text Color',
     builder: (context) {
-      const color = Colors.blue;
+      const backgroundColor = Colors.white;
+      const foregroundColor = Colors.blue;
 
-      return Center(
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(backgroundColor: color),
+      return CodeSnippet(
+        code: '''WraflButton(
+          label: 'Blue Text Color in white',
+          backgroundColor: backgroundColor,
+          textColor: foregroundColor,
           onPressed: () {
             ScaffoldMessenger.of(context).showSnackBar(
               const SnackBar(content: Text('Button Pressed!')),
             );
           },
-          child: const Text('Press Me!'),
+        )''',
+        child: WraflButton(
+          label: 'Blue Text Color in white',
+          backgroundColor: backgroundColor,
+          textColor: foregroundColor,
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Button Pressed!')),
+            );
+          },
+        ),
+      );
+    },
+  ),
+  Story(
+    name: 'Call To Action/WraflButton/Left Icon',
+    builder: (context) {
+      const backgroundColor = Colors.white;
+      const foregroundColor = Colors.blue;
+
+      return CodeSnippet(
+        code: '''WraflButton(
+          label: 'Clipboard',
+          hideLabel: true,
+          leftIcon: const WraflIcon(
+              icon: FontAwesomeIcons.clipboard, color: Colors.blue),
+          backgroundColor: backgroundColor,
+          textColor: foregroundColor,
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Button Pressed!')),
+            );
+          },
+        )''',
+        child: WraflButton(
+          label: 'Clipboard',
+          hideLabel: true,
+          leftIcon: const WraflIcon(
+              icon: FontAwesomeIcons.clipboard, color: Colors.blue),
+          backgroundColor: backgroundColor,
+          textColor: foregroundColor,
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Button Pressed!')),
+            );
+          },
+        ),
+      );
+    },
+  ),
+  Story(
+    name: 'Call To Action/WraflButton/Left Icon + Label',
+    builder: (context) {
+      const backgroundColor = Colors.white;
+      const foregroundColor = Colors.blue;
+
+      return CodeSnippet(
+        code: '''WraflButton(
+          label: 'Search',
+          leftIcon: const WraflIcon(
+              icon: FontAwesomeIcons.magnifyingGlass, color: Colors.blue),
+          backgroundColor: backgroundColor,
+          textColor: foregroundColor,
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Button Pressed!')),
+            );
+          },
+        )''',
+        child: WraflButton(
+          label: 'Search',
+          leftIcon: const WraflIcon(
+              icon: FontAwesomeIcons.magnifyingGlass, color: Colors.blue),
+          backgroundColor: backgroundColor,
+          textColor: foregroundColor,
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Button Pressed!')),
+            );
+          },
+        ),
+      );
+    },
+  ),
+  Story(
+    name: 'Call To Action/WraflButton/With Spinner',
+    builder: (context) {
+      const backgroundColor = Colors.white;
+      const foregroundColor = Colors.blue;
+
+      return CodeSnippet(
+        code: '''WraflButton(
+          label: 'Searching...',
+          backgroundColor: backgroundColor,
+          textColor: foregroundColor,
+          showSpinner: true,
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Button Pressed!')),
+            );
+          },
+        )''',
+        child: WraflButton(
+          label: 'Searching...',
+          backgroundColor: backgroundColor,
+          textColor: foregroundColor,
+          showSpinner: true,
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Button Pressed!')),
+            );
+          },
+        ),
+      );
+    },
+  ),
+  Story(
+    name: 'Call To Action/WraflButton/Right Icon',
+    builder: (context) {
+      const backgroundColor = Colors.white;
+      const foregroundColor = Colors.blue;
+
+      return CodeSnippet(
+        code: '''WraflButton(
+          label: 'Actions',
+          backgroundColor: backgroundColor,
+          textColor: foregroundColor,
+          rightIcon: const WraflIcon(
+              icon: FontAwesomeIcons.chevronDown, color: Colors.blue),
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Button Pressed!')),
+            );
+          },
+        )''',
+        child: WraflButton(
+          label: 'Actions',
+          backgroundColor: backgroundColor,
+          textColor: foregroundColor,
+          rightIcon: const WraflIcon(
+              icon: FontAwesomeIcons.chevronDown, color: Colors.blue),
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Button Pressed!')),
+            );
+          },
         ),
       );
     },
